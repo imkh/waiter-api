26b4fae85e638c7521c72948c20016c194d8a38b
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-05-30 02:14:03 -0700

    Revert "fix param variable in route logout"
    This reverts commit b1d8275ab625bbe9ab6cb9ac6d489d8236fbd782.

--- routes/user.js
+++ routes/user.js
@@ -241,14 +241,14 @@ router.post('/login', function(req, res) {
 router.put('/:id/logout', function(req, res) {
     var causes = [];
 
-    mongoose.model('User').findById(req.params.id, function (err, user) {
+    mongoose.model('User').findById(req.id, function (err, user) {
         if (err) {
             res.status(httpCodes.internalServerError).jsend.error({message: err.message});
             return ;
         }
         if (user === null) {
             causes.push('User not found');
-            res.status(httpCodes.notFound).jsend.fail({message: 'User logout failed', causes: causes});
+            res.status(httpCodes.notFound).jsend.fail({message: 'Logout failed', causes: causes});
             return ;
         }
 
