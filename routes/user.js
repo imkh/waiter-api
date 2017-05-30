933bb6768ccaf077684ef3cc8417d993dd4f45fb
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-05-30 02:15:50 -0700

    put all users in an array (route get all users)

--- routes/user.js
+++ routes/user.js
@@ -273,7 +273,7 @@ router.get('/', function(req, res) {
             res.status(httpCodes.internalServerError).jsend.error({message: err.message});
             return ;
         }
-        res.jsend.success({users: users});
+        res.jsend.success(users);
     }).select('-password -__v');
 });
 // End: Unprotected routes
