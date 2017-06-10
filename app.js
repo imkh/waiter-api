04b48ed30b5557d4f1a27c1142b3fbbc53dcffee
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-06-10 15:02:38 -0700

    add dotenv

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ var config = require('config');
 var http = require('./http');
 /* var io = require('socket.io')(http);*/
 
-
+var dotenv = require('dotenv');
+// There's no need to check if .env exists, dotenv will check this // for you. It will show a small warning which can be disabled when // using this in production.
+dotenv.load();
 
 const serverConfig = config.get('server');
 
